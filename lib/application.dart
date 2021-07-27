664976a5287f397cc1638c690d0063997a68bf3c
the1coder-pro <mohamed2015pre@gmail.com>
2021-07-27 09:29:14 +0300

    added the new fields in the DataBase

--- lib/application.dart
+++ lib/application.dart
@@ -1,22 +1,38 @@
-// String dbName = 'flutter_contacts.db';
-String dbName = 'the_contacts.db';
+String dbName = 'the_cadah_contacts.db';
 int dbVersion = 1;
-
-// firstName TEXT (changed to name)
-// lastName TEXT (removed)
 
 List<String> dbCreate = [
   // the List
   """
   CREATE TABLE contacts (
     id INTEGER PRIMARY KEY, 
-    name TEXT NOT NULL,
-    phoneNumber TEXT,
-    nationalId TEXT UNIQUE NOT NULL,
-    helpDate TEXT,
-    helpType TEXT,
-    helpAmount INTEGER,
-    helpDuration TEXT,
+    DeadPersonName TEXT NOT NULL,
+
+    RequesterName TEXT NOT NULL,
+    RequesterPhoneNumber TEXT,
+    RequesterNationalId TEXT UNIQUE NOT NULL,
+
+    ProviderName TEXT NOT NULL,
+    ProviderPhoneNumber TEXT,
+    ProviderReceivedDate TEXT,
+    
+    HelpDuration TEXT,
+
+    HajjTypeAmount INTEGER,
+    HajjTypeTimes INTEGER,
+
+    NadharTypeAmount INTEGER,
+    NadharTypeTimes INTEGER,
+
+    PrayerTypeAmount INTEGER,
+    PrayerTypeTimes INTEGER,
+
+    FastTypeAmount INTEGER,
+    FastTypeTimes INTEGER,
+
+    AmalBrTypeAmount INTEGER,
+    AmalBrTypeTimes INTEGER,
+
     notes TEXT,
     favorite INTEGER
   )""",
